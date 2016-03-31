63ce1486e9548db8cb70ce902435f3e025a77a78
Rémy Greinhofer <remy.greinhofer@gmail.com>
2016-03-30 23:04:35 -0500

    Update production settings according to 12factor

--- craigomatic/settings/production.py
+++ craigomatic/settings/production.py
@@ -1,11 +1,15 @@
 from .base import *  # noqa
 import os
 
+import dj_database_url
+
 DEBUG = False
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
-DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2', }}
+# Update database configuration with ${DATABASE_URL}.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
