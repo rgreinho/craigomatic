a89afbd8e4bf317e19e32760a4a5da99878c1547
Rémy Greinhofer <remy.greinhofer@gmail.com>
2016-03-30 22:49:35 -0500

    Update production settings

--- craigomatic/settings/production.py
+++ craigomatic/settings/production.py
@@ -1,21 +1,11 @@
 from .base import *  # noqa
 import os
 
-
 DEBUG = False
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'craigomatic',
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', ''),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-    }
-}
+DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2', }}
