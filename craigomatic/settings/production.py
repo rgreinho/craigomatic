17a574ac15822b5c1e87c959185d3517e89492c5
Rémy Greinhofer <remy.greinhofer@gmail.com>
2016-03-30 22:09:28 -0500

    Add production settings
    Add production settings getting populated by environment variables.

---
+++ craigomatic/settings/production.py
@@ -0,0 +1,21 @@
+from .base import *  # noqa
+import os
+
+
+DEBUG = False
+TEMPLATES['OPTIONS']['debug'] = DEBUG
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'craigomatic',
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
+    }
+}
